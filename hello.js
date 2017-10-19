14f0c80b5137ee339be654ff794d0234ac1c401b
Robert P <robisys@users.noreply.github.com>
2017-10-19 16:27:58 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+console.log("start");
 
 var http = require('http');
 http.createServer(function (req, res) {
@@ -6,3 +7,4 @@ http.createServer(function (req, res) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
+console.log("ende");
